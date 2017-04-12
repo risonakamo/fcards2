aff51e192137dbc7b1f40a49bb4f1f26f6c15aa3
risonakamo <hmykanae@gmail.com>
2017-04-12 19:10:57 -0400

    fixes n stuff

--- main.js
+++ main.js
@@ -6,9 +6,8 @@ function main()
 
     var a=new fCard();
 
-    a.toptext="3";
+    a.toptext="3 what rings u got bnitc-acosaihd fw4kj3k4 tklj lkjer ";
     a.bottext="c";
 
     fcards.appendChild(a);
-
 }
