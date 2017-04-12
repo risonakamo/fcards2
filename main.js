16a14905a0cc8299ed55c000ebca89f6f95ff972
risonakamo <hmykanae@gmail.com>
2017-04-12 15:32:14 -0400

    +weird data binding stuff

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ window.onload=main;
 
 function main()
 {
+    var fcards=document.querySelector(".fcards");
 
+    var a=new fCard();
+
+    a.toptext="3";
+    a.bottext="c";
+
+    fcards.appendChild(a);
 }
