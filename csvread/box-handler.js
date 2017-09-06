09ab30f96676c04e1e4767d9a748f06439eab663
risonakamo <hmykanae@gmail.com>
2017-09-06 12:01:52 -0400

    box handler object csv works

---
+++ csvread/box-handler.js
@@ -0,0 +1,52 @@
+class _boxhandler
+{
+    constructor()
+    {
+        this.outputArea=document.querySelector(".output-box");
+        this.outputCover=this.outputArea.querySelector(".cover");
+        this.outputText=this.outputArea.querySelector(".output-area");
+
+        this.inputArea=document.querySelector(".input-area");
+
+        this.outputCover.addEventListener("click",(e)=>{
+            this.parse();
+        });
+
+        this.inputArea.addEventListener("keydown",(e)=>{
+            if (e.key=="Enter" && e.ctrlKey)
+            {
+                e.preventDefault();
+                this.parse();
+            }
+        });
+    }
+
+    parse()
+    {
+        this.outputText.value=this.parseInput();
+        this.outputArea.classList.remove("small");
+    }
+
+    parseInput()
+    {
+        var data=this.inputArea.value;
+
+        data=data.split("\n");
+
+        var y;
+        for (var x=0,l=data.length;x<l;x++)
+        {
+            data[x]=data[x].match(/("(.+?)")|([^,]+)/g);
+
+            for (y=0;y<2;y++)
+            {
+                if (data[x][y][0]==`"`)
+                {
+                    data[x][y]=data[x][y].slice(1,-1);
+                }
+            }
+        }
+
+        return JSON.stringify({boxes:data});
+    }
+}
