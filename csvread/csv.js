fca33ac3f6033d046da41f82db02566e232ca820
risonakamo <hmykanae@gmail.com>
2017-09-05 22:23:19 -0400

    working on csv converter for quick card json creation

---
+++ csvread/csv.js
@@ -0,0 +1,32 @@
+window.onload=main;
+
+function main()
+{
+
+}
+
+function parseInput()
+{
+    var data=document.querySelector(".input-area");
+
+    data=data.value;
+
+    data=data.split("\n");
+
+    var y;
+    for (var x=0,l=data.length;x<l;x++)
+    {
+        data[x]=data[x].match(/("(.+?)")|([^,]+)/g);
+
+        for (y=0;y<2;y++)
+        {
+            if (data[x][y][0]==`"`)
+            {
+                data[x][y]=data[x][y].slice(1,-1);
+            }
+        }
+
+    }
+
+    return JSON.stringify({cards:data});
+}
